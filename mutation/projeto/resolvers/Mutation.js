eab80a84462cf02afa66ed18184f02f26e769db1
Mariana Bastos <maribastossrodrigues@gmail.com>
2022-07-31 17:18:40 -0300

    add create and update user mutations

--- mutation/projeto/resolvers/Mutation.js
+++ mutation/projeto/resolvers/Mutation.js
@@ -1,18 +1,51 @@
 const { usuarios, proximoId } = require('../data/db');
 
 module.exports = {
-    novoUsuario(_, { nome, email, idade }) {
+    // { nome, email, idade }
+    novoUsuario(_, args) {
+        const emailExistente = usuarios.some(u => u.email === email);
+
+        if(emailExistente) {
+            throw new Error('E-mail já cadastrado');
+        }
+
         const novo = {
             id: proximoId(),
-            nome,
-            email,
-            idade,
+            ...args,
             perfil_id: 1,
             status: 'ATIVO'
         };
 
         usuarios.push(novo);
         return novo;
-    }
+    },
+    excluirUsuario(_, { id }) {
+        const i = usuarios.findIndex(u => u.id === id);
+        if(i < 0) return null;
+        const excluidos = usuarios.splice(i, 1);
+        return excluidos ? excluidos[0] : null;
+    },
+    alterarUsuario(_, args) {
+        const i = usuarios.findIndex(u => u.id === args.id);
+        if(i < 0) return null;
 
+        // 2ª forma de fazer
+        usuarios[i].nome = args.nome;
+        usuarios[i].email = args.email;
+        
+        if(args.idade) {
+            usuarios[i].idade = args.idade;
+        }
+        
+        return usuarios[i];
+
+        // 1ª forma de fazer
+        // const usuario = {
+        //     ...usuarios[i],
+        //     ...args
+        // };
+
+        // usuarios.splice(i, 1, usuario);
+        // return usuario;
+    }
 }
