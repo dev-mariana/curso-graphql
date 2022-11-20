ecfdf53ba661f93889ab344466de8a5bc26b258c
mariana_bastos <maribastossrodrigues@gmail.com>
2022-11-20 11:39:08 -0300

    add parte ll do desafio - salvarPerfil

--- banco-de-dados/projeto/testes/desafio.js
+++ banco-de-dados/projeto/testes/desafio.js
@@ -19,12 +19,27 @@ async function salvarUsuario(nome, email, senha) {
         })
 }
 
+async function salvarPerfil(nome, rotulo) {
+    const perfilExistente = await db('perfis').where({ nome });
+    console.log(perfilExistente)
+    
+    if (perfilExistente.length === 0) {
+        return db('perfis').insert({ nome, rotulo })
+    }
+
+    return await db('perfis').where({ nome }).update({ rotulo })
+}
+
 async function executar() {
     const usuario = await salvarUsuario('Mari', 'm@gmail.com', '123');
     const usuario2 = await salvarUsuario('Mari2', 'm@gmail.com', '1234');
+    const perfil = await salvarPerfil('admin', 'Administrador');
+    const perfil2 = await salvarPerfil('admin', 'Administrador2');
 
     console.log(usuario)
     console.log(usuario2)
+    console.log(perfil)
+    console.log(perfil2)
 }
 
 executar()
